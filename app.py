78c328aa399292fb5db9e7d3bf86fac2731ed54b
Joni <hangjoni@gmail.com>
2024-03-13 16:31:41 +0800

    built front end for prediction

--- app.py
+++ app.py
@@ -13,21 +13,48 @@ from utils import *
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# end point to serve front end prediction with one input
+@app.route('/', methods=['GET', 'POST'])
+def predict_one_datapoint():
+    if request.method=='GET':
+        return render_template('index.html')
+    else:
+        data = {
+            'gender': int(request.form.get('gender')),
+            'age': int(request.form.get('age')),
+            'hypertension': int(request.form.get('hypertension')),
+            'heart_disease': int(request.form.get('heart_disease')),
+            'work_type': int(request.form.get('work_type')),
+            'avg_glucose_level': float(request.form.get('avg_glucose_level')),
+            'bmi': float(request.form.get('bmi'))
+        }
+        preds, probs = _predict_helper(data)
+        pred, prob = preds[0], probs[0]
 
+        return render_template('index.html', pred=pred, prob=prob)
+
+# end point for batch prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    json_ = request.get_json()
+    json_data = request.get_json()
 
-    query = pd.DataFrame(json_)
-    preds = model.predict(query)
-    probs = model.predict_proba(query)[:,1]
+    preds, probs = _predict_helper(json_data)
     
     df = pd.DataFrame({'pred': preds, 'prob': probs})
 
     return df.to_json()
+
+def _predict_helper(json_data):
+    print('json_data')
+    print(json_data)
+    # json_data could be dictionary or list of dictionary
+    if isinstance(json_data, list):
+        query = pd.DataFrame(json_data)
+    else:
+        query = pd.DataFrame([json_data])
+    preds = model.predict(query)
+    probs = model.predict_proba(query)[:,1]
+    return preds, probs
 
 
 if __name__ == '__main__':
